40680ca6d4e8b4b9ac01a533ae85427f9d216da6
alibama <adp6j@virginia.edu>
2021-06-25 15:29:07 +0000

    CCI credit score improvement project

--- opendata.py
+++ opendata.py
@@ -2,7 +2,6 @@ import math
 import streamlit as st
 import numpy as np
 import json
-#import rdflib
 import altair as alt
 from urllib.request import urlopen
 #from xml.etree.ElementTree import parse
@@ -21,13 +20,14 @@ from pandas_profiling import ProfileReport
 import streamlit.components.v1 as components
 from streamlit_pandas_profiling import st_profile_report
 
-# Custome Component Fxn
+# Custom Component Fxn
 import sweetviz as sv 
 
+st.header('Open Data Dashboard from EuropePMC ')
+st.subheader('Exploratory Data Analysis with Streamlit')
 
+st.markdown('In this app we are using content pulled from the [EuropePMC](https://europepmc.org/RestfulWebService) with a small Python script, gratefully edited by Dr. Maaly Nassar of the EuropePMC publication team, and served via [Streamlit](https://streamlit.io)')
 
-#fulltext_list
-#'''hello amelia!  it is very nice to see you today'''
 
 @st.cache(suppress_st_warning=True)
 def bigask ():
@@ -39,7 +39,7 @@ def bigask ():
     nxt_mrk = '*' #next cursor mark
     while cr_mrk != nxt_mrk:              
         url = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?'
-        query = '(AFF:"University of Virginia") AND (FIRST_PDATE:[2017-01-01 TO 2020-12-31])'
+        query = '(AFF:"University of Virginia") AND (FIRST_PDATE:[2000-01-01 TO 2020-12-31]) AND (AUTH:"Wladek Minor")'
         params = {'query':query, 'resultType':'core', 'synonym':'TRUE','cursorMark':nxt_mrk,'pageSize':'1000','format':'json'}
         response = requests.get(url,params)
         rjson = response.json()
@@ -53,73 +53,33 @@ def bigask ():
             dct['id'].append(rslt['id']) if 'id' in rslt.keys() else dct['id'].append(0)
             dct['oa'].append(rslt['isOpenAccess']) if 'isOpenAccess' in rslt.keys() else dct['oa'].append(0)
             dct['cited'].append(rslt['citedByCount']) if 'citedByCount' in rslt.keys() else dct['cited'].append(0) 
-            dct['aff'].append(rslt['affiliation']) if 'affiliation' in rslt.keys() else dct['aff'].append(0)
-            #dct['journal'].append(rslt['journalInfo']['journal']['title']) if ['journalInfo']['journal']['title'] in rslt.keys() else dct['journal'].append(0)  
+            dct['aff'].append(rslt['affiliation']) if 'affiliation' in rslt.keys() else dct['aff'].append(0) 
     df=pd.DataFrame.from_dict(dct, orient='columns')
-        #print(dct)
     return df
 
-def bigask2 ():
-    dct = {}
-    for col in ['oa','author','year','title','doi','id','cited','aff']:
-        dct[col] = []
 
-    cr_mrk= '' #current cursor mark
-    nxt_mrk = '*' #next cursor mark
-    while cr_mrk != nxt_mrk:              
-        url = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?'
-        query = '(AFF:"Virginia Tech") AND (FIRST_PDATE:[2017-01-01 TO 2020-12-31])'
-        params = {'query':query, 'resultType':'core', 'synonym':'TRUE','cursorMark':nxt_mrk,'pageSize':'1000','format':'json'}
-        response = requests.get(url,params)
-        rjson = response.json()
-        cr_mrk = urlparse.unquote(rjson['request']['cursorMark'])
-        nxt_mrk = urlparse.unquote(rjson['nextCursorMark'])
-        for rslt in rjson['resultList']['result']:
-            dct['author'].append(rslt['authorString']) if 'authorString' in rslt.keys() else dct['author'].append(0)
-            dct['year'].append(rslt['pubYear']) if 'pubYear' in rslt.keys() else dct['year'].append(0)
-            dct['title'].append(rslt['title']) if 'title' in rslt.keys() else dct['title'].append(0)
-            dct['doi'].append(rslt['doi']) if 'doi' in rslt.keys() else dct['doi'].append(0)
-            dct['id'].append(rslt['id']) if 'id' in rslt.keys() else dct['id'].append(0)
-            dct['oa'].append(rslt['isOpenAccess']) if 'isOpenAccess' in rslt.keys() else dct['oa'].append(0)
-            dct['cited'].append(rslt['citedByCount']) if 'citedByCount' in rslt.keys() else dct['cited'].append(0) 
-            dct['aff'].append(rslt['affiliation']) if 'affiliation' in rslt.keys() else dct['aff'].append(0)
-            #dct['journal'].append(rslt['journalInfo']['journal']['title']) if ['journalInfo']['journal']['title'] in rslt.keys() else dct['journal'].append(0)  
-    df2=pd.DataFrame.from_dict(dct, orient='columns')
-        #print(dct)
-    return df2
+    
 
 #menu = ["Y", "N"]
 #st.sidebar.subheader("Select Option")
 #choice = st.sidebar.selectbox("Full Text", menu)
 
 dfdata=bigask()
-dfdata2=bigask2()
+#dfdata2=bigask2()
 
 #dfdata= dfdata[dfdata['oa'] == choice] 
 #df=pd.DataFrame.from_dict(rslt)        
 
-citations = st.sidebar.slider('Number of citations', 0, 100, 1)
-#citations = 0
-dfdata = dfdata[dfdata['cited'] >= citations] 
-dfdata['doi'] = dfdata['doi'].astype(str)  #pandas was calling this a mixed type column and it borked sweetviz
-dfdata['aff'] = dfdata['aff'].astype(str)  #pandas was calling this a mixed type column and it borked sweetviz
-#dfdata
-dfdata.to_csv('opendata.csv', index=False)
-st.write(dfdata)
-        
 
 
-#openFilter = sorted(df['openAccess'].drop_duplicates()) # select the open access values 
+
+#openFilter = sorted(df['aff'].drop_duplicates()) # select the open access values 
 #open_Filter = st.sidebar.selectbox('Open Access?', openFilter) # render the streamlit widget on the sidebar of the page using the list we created above for the menu
 #df2=df[df['openAccess'].str.contains(open_Filter)] # create a dataframe filtered below
 #st.write(df2.sort_values(by='date'))
 
 
 
-
-valLayer = alt.Chart(dfdata).mark_bar().encode(x='year',y='count(oa)',color='oa')#
-
-st.altair_chart(valLayer, use_container_width=True)
 
 
 
@@ -129,11 +89,12 @@ def st_display_sweetviz(report_html,width=1000,height=500):
 	components.html(page,width=width,height=height,scrolling=True)
 
 
+    
 def main():
 	"""A Simple EDA"""
 
-	menu = ["Home","Pandas Profile","Sweetviz","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
+	menu = ["Pandas Profile","Sweetviz"]
+	choice = st.selectbox("Choose an EDA report type",menu)
 
 	if choice == "Pandas Profile":
 		st.subheader("Automated EDA with Pandas Profile")
@@ -148,9 +109,22 @@ def main():
 			report.show_html()
 			st_display_sweetviz("SWEETVIZ_REPORT.html")
 
+st.subheader('Streamlit makes interactive widgets with minimal code')
+    
+'''This is a simple slider built in streamlit that interacts with the imported data and provides the user with a textual and graphical output'''
+#citations = st.slider('Number of citations', 0, 100, 1)
+citations = 0
+dfdata = dfdata[dfdata['cited'] >= citations] 
+dfdata['doi'] = dfdata['doi'].astype(str)  #pandas was calling this a mixed type column and it borked sweetviz
+dfdata['aff'] = dfdata['aff'].astype(str)  #pandas was calling this a mixed type column and it borked sweetviz
+#dfdata
+dfdata.to_csv('opendata.csv', index=False)
+st.write(dfdata)
+        
+valLayer = alt.Chart(dfdata).mark_bar().encode(x='year',y='count(oa)',color='oa')
+st.altair_chart(valLayer, use_container_width=True)
+st.subheader('EDA reports provide a simple & low-code overview of data')
+'''The questions one may ask are reasonably constrained by the data one has access to. Exploratory data analysis (EDA), aims to help establish the type and quality of the data to be processed and in our examples applies univariate graphical and textual reports to give data sets a first review.  In this case these reports are generated with only a few lines of code, and are thus especially useful for programmers to deliver to stakeholders early in the process'''
+
 if __name__ == '__main__':
 	main()
-    
-
-
-st.markdown('Download a copy of the data [opendata.csv](https://tljh.codeforcville.org/user/alibama/doc/tree/opendata.csv)')
